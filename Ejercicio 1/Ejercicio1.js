6e28f207ea03abf83a5dd74a01e32741944748ab
Fran088 <82686722+Fran088@users.noreply.github.com>
2021-06-01 08:36:28 -0300

    Update Ejercicio1.js

--- Ejercicio 1/Ejercicio1.js
+++ Ejercicio 1/Ejercicio1.js
@@ -1,10 +1,10 @@
-//1. Pide una nota (número). Muestra la calificación según la nota:
-//○ 0-3: Muy deficiente
-//○ 3-5: Insuficiente
-//○ 5-6: Suficiente
-//○ 6-7: Bien
-//○ 7-9: Notable
-//○ 9-10: Sobresaliente
+/*1. Pide una nota (número). Muestra la calificación según la nota:
+○ 0-3: Muy deficiente
+○ 3-5: Insuficiente
+○ 5-6: Suficiente
+○ 6-7: Bien
+○ 7-9: Notable
+○ 9-10: Sobresaliente*/
 
 const nota = prompt("Ingrese la nota (entre 0 y 10): ");
     if(nota>0&&nota<=3){
